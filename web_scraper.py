2f262724bf871097942f9a162eae08cd8cef57e9
Hadi Asyrafi <hadiasyrafi94@gmail.com>
2017-03-23 20:58:15 +0800

    Backup 23_3_2017

--- web_scraper.py
+++ web_scraper.py
@@ -1,17 +1,16 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-url = "http://www.thestar.com.my/news/nation/2017/03/22/lima-2017-malaysia-officially-receives-its-latest-rmaf-a400m-aircraft/"
+#url = "http://www.thestar.com.my/news/nation/2017/03/22/lima-2017-malaysia-officially-receives-its-latest-rmaf-a400m-aircraft/"
 
-page = urllib2.urlopen(url)			#query website and return html
+#page = urllib2.urlopen(url)			#query website and return html
 
-soup = BeautifulSoup(page, 'html.parser')	#parse html in BS format
+soup = BeautifulSoup('<b> class="boldest">Extremely Bold</b>')	#parse html in BS format
 
 #print soup.prettify()				#look at html structure
+#print soup.script
+#for link in soup.find_all('a'):
+#	print link.get('href')
 
-print soup.script
-
-for link in soup.find_all('a'):
-	print link.get('href')
-
-print soup.get_text()
+tag = soup.b
+print type(tag)
