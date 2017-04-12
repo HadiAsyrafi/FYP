4341b2fb168aacb17f2f7e3187bae7dcd259d587
Hadi Asyrafi <hadiasyrafi94@gmail.com>
2017-04-13 00:39:41 +0800

    Making gui for main window

--- tool_gui.py
+++ tool_gui.py
@@ -6,9 +6,11 @@ class mainGui:
     def __init__(self, master):
         self.master = master
         master.title("Wikipedia Tools")
+	root.resizable(width=False, height=False)
+	master.geometry('860x540')
 
 	self.mainframe = ttk.Frame(master, padding="3 3 12 12")
-	self.mainframe.pack()
+	self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
         self.label = ttk.Label(self.mainframe, text="This is our first GUI!")
         self.label.pack()
