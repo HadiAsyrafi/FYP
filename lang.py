62575e015ebf677864770845ad4056924c564060
Hadi Asyrafi <hadiasyrafi94@gmail.com>
2017-05-04 13:48:58 +0800

    add language detection

--- lang.py
+++ lang.py
@@ -36,7 +36,14 @@ def _calculate_languages_ratios(text):
 
     # Compute per language included in nltk number of unique stopwords appearing in analyzed text
     for language in stopwords.fileids():
+    # [danish, dutch, english, finnish, french, german, hungarian, italian, kazakh, norwegian, portuguese, russian, spanish, swedish, turkish]
+
         stopwords_set = set(stopwords.words(language))
+	'''stops=[]
+	for item in stopwords.words(language):
+		stops.extend(wordpunct_tokenize(item))
+		stopwords_set = set(stops)'''
+
         words_set = set(words)
         common_elements = words_set.intersection(stopwords_set)
 
@@ -63,6 +70,8 @@ def detect_language(text):
 
     ratios = _calculate_languages_ratios(text)
 
+    print ratios
+
     most_rated_language = max(ratios, key=ratios.get)
 
     return most_rated_language
@@ -72,34 +81,11 @@ def detect_language(text):
 if __name__=='__main__':
 
     text = '''
-    Er war sanftmütig und freundlich. Seine Augen standen dicht beieinander. Das bedeutete
-Hinterlist. Seine Brauen stießen über der Nase zusammen. Das bedeutete Jähzorn. Seine Nase
-war lang und spitz. Das bedeutete unstillbare Neugier. Seine Ohrläppchen waren
-angewachsen. Das bedeutete Hang zum Verbrechertum. Warum gehst du nicht unter die
-Leute?, fragte man ihn. Er besah sich im Spiegel und bemerkte einen grausamen Zug um
-seinen Mund. Ich bin kein guter Mensch, sagte er. Er verbohrte sich in seine Bücher. Als er
-sie alle ausgelesen hatte, musste er unter die Leute, sich ein neues Buch kaufen gehn.
-Hoffentlich gibt es kein Unheil, dachte er und ging unter die Leute. Eine Frau sprach ihn an
-und bat ihn, ihr einen Geldschein zu wechseln. Da sie sehr kurzsichtig war, musste sie
-mehrmals hin- und zurücktauschen. Der Skorpion dachte an seine Augen, die dicht
-beieinander standen, und verzichtete darauf, sein Geld hinterlistig zu verdoppeln. In der
-Straßenbahn trat ihm ein Fremder auf die Füße und beschimpfte ihn in einer fremden Sprache.
-Der Skorpion dachte an seine zusammengewachsenen Augenbrauen und ließ das Geschimpfe,
-das er nicht verstand, als Bitte um Entschuldigung gelten. Er stieg aus und vor ihm lag eine
-Brieftasche auf der Straße. Der Skorpion dachte an seine Nase und blickte sich nicht und
-drehte sich auch nicht um. In der Buchhandlung fand er ein Buch, das hätte er gern gehabt.
-Aber es war zu teuer. Es hätte gut in seine Manteltasche gepasst. Der Skorpion dachte an
-seine Ohrläppchen und stellte das Buch ins Regal zurück. Er nahm ein anderes. Als er es
-bezahlen wollte, klagte ein Bücherfreund: Das ist das Buch, das ich seit Jahren suche. Jetzt
-kauft's mir ein anderer weg. Der Skorpion dachte an den grausamen Zug um seinen Mund und
-sagte: Nehmen Sie das Buch. Ich trete zurück. Der Bücherfreund weinte fast. Er presste das
-Buch mit beiden Händen an sein Herz und ging davon. Das war ein guter Kunde, sagte der
-Buchhändler, aber für Sie ist auch noch was da. Er zog aus dem Regal das Buch, das der
-Skorpion so gern gehabt hätte. Der Skorpion winkte ab: Das kann ich mir nicht leisten. -
-Doch, Sie können, sagte der Buchhändler, eine Liebe ist der anderen wert. Machen Sie den
-Preis. Der Skorpion weinte fast. Er presste das Buch mit beiden Händen fest an sein Herz und,
-da er nichts mehr frei hatte, reichte er dem Buchhändler zum Abschied seinen Stachel. Der
-Buchhändler drückte den Stachel und fiel tot um.
+    Asielzoekerscentra worden jaarlijks geconfronteerd met vermissingen van AMA (Alleenstaande Minderjarige Asielzoekers) jongens en meisjes. Volgens vele Asielzoekers centra worden er veelal meisjes vermist. In 1990 zouden er 3 per jaar mysterieus verdwijnen en in de jaren daaropvolgend zijn dat er tientallen geworden. Langzaam begint de groepsleiding te begrijpen, hoe dit zoal in zijn werk gaat, maar ervaren een bureaucratische muur waar met geen mogelijkheid verandering in gebracht kan worden. Het schrijnende is dat de groepsleiding hier dagelijks mee te maken krijgen maar verzuipen in de tekortgeschoten protocollen. Het is dan ook verontrustend dat het asielzoekersbeleid niet verantwoord versterkt wordt, zodat deze kwetsbare groep kinderen buiten de mazen van de wet vallen.
+ 
+De vele artikelen die hierover zijn geschreven, geven aan, dat het om een weldoordacht netwerk gaat. Dit begint al in de landen van oorsprong waar ze de meisjes ronselen. Ze worden verkracht of bedreigd dat hun dierbaren worden vermoord. Kansloos als ze zijn en volgepropt met kortzichtige moralen door hun omgeving, vinden ze geen mogelijkheid om uit de handen van de pooiers te blijven. Wanneer ze eenmaal afhankelijk zijn van de pooier, krijgen ze een paspoort om bijvoorbeeld naar Nederland te vluchten. Ze weten niet, dat zelfs het AZC niet veilig is en de begeleiding kan niets doen om de verdwijningen te voorkomen. De meisjes hebben een telefoonnummer, dat moeten ze bellen want anders gebeurt er waar ze zo bang voor zijn. Er zijn meisjes die het telefoonnummer zijn kwijtgeraakt en daardoor in een psychiatrische inrichting terecht zijn gekomen. Een niet te onderschatten angst waardoor het heel lastig is om ze te helpen. Soms lopen de meisjes zelf weg maar vaak worden ze ontvoerd, gezien het feit dat ze alles hebben achtergelaten, ook al is dat niet veel.
+ 
+Dit netwerk zit diepgeworteld in vele legale instanties, men moet dan denken aan ambassades, ministerie van Justitie, vliegtuigmaatschappijen en politie (vreemdelingen- en zedenpolitie). Vele honderden meisjes en een aantal jongens zijn verdwenen en er lijkt geen einde aan te komen. Alle verontruste berichten vanuit alle hoeken en instanties, verontrusten niet genoeg om tot een verbetering van het systeem te komen.
     '''
 
     language = detect_language(text)
